3045f858a9f44a32595ea53f312b41d0699950af
Michael Rehani <90718054+mrrehani@users.noreply.github.com>
2022-06-20 18:06:51 -0500

    Add files via upload

--- TemperatureGrapher.py
+++ TemperatureGrapher.py
@@ -5,6 +5,8 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.linear_model import LinearRegression
+
+
 class TemperatureGrapher():
     #This function groups the country data by year
     def group_by_year(self, DF, column):  
@@ -35,8 +37,8 @@ class TemperatureGrapher():
         return (dict_years)
 
 
-    def graph_data(self, data, dataColumn,uncertaintyColumn, type_data):    
-        data, uncertainty = self.group_by_year(data,dataColumn), self.group_by_year(data, uncertaintyColumn)
+    def graph_data(self, regionNames, data, tempAx, uncertainAx, row,col):    
+        data, uncertainty = self.group_by_year(data,"AverageTemperature"), self.group_by_year(data, "AverageTemperatureUncertainty")
         #The code below creating a linear regression line is from https://realpython.com/linear-regression-in-python/
         #Creating a dataframe from the yearly averages and formatting it properly. 
         all_centuries_df=DataFrame([data]) #Data is a dict of the average temperature each year.
@@ -54,15 +56,17 @@ class TemperatureGrapher():
         all_centuries_df["year"]=all_centuries_df["year"].astype("int64")#Setting the years column to type int. 
         #The line of code above is required for the next line to work, as the years column was previously type str.
         all_centuries_df["fit"] = (all_centuries_df["year"] * float(slope)) + float(intercept)#Obtains y values through the formula y=mx+b.
-        fig, axs = plt.subplots(2,1,figsize=(7.5,7.5)) 
-        plt.subplots_adjust(hspace=.33)
-        plot_area=all_centuries_df.plot.line(x='year', y='fit', c='purple', ax=axs[0])
+
+        plot_area = all_centuries_df.plot.line(x='year', y='fit', c='purple', ax=tempAx[row,col])
         #In order to color-code the graph, I need to group the data by the century.
+
         list_centuries=[]
+        
         for entry in all_centuries_df["year"]:
             year=str(entry) #Above I changed the entries in the year column to int. However in order to execute the line below
             #it needs to be type str
             list_centuries.append(year[:2]+"00s") #This way, entries from the years 1752 1713 1763 1794 will all be grouped as "1700s" (for example).
+        
         all_centuries_df["century"]=list_centuries #Adds an extra column to the dataframe denoting which century each entry is classified into.
         colors = ["blue", "green", "orange", "red"]
         markers = ["o", "^", "v", ","]
@@ -71,29 +75,26 @@ class TemperatureGrapher():
         #The loop is copied from a lecture in the class Data Programming 220 at UW Madison
         for variety in centuries: #variety is each century, so it is '1700s', '1800s', etc.
             sub_df = all_centuries_df[all_centuries_df["century"]==variety] #Creating a temporary dataframe containing all entries from the 1700s, then the 1800s, etc. 
-            plot_area = sub_df.plot.scatter(x="year", y="temp",ax=axs[0], #Plots all data from the current century using the first entry in the color and label list, then removing that entry. 
+            plot_area = sub_df.plot.scatter(x="year", y="temp",ax=tempAx[row,col], #Plots all data from the current century using the first entry in the color and label list, then removing that entry. 
                                             color=colors.pop(0),
                                             label=variety, marker=markers.pop(0)) 
-
+        
         plot_area.set_xlabel("Year")
         plot_area.set_ylabel("Temperature")
-        plot_area.set_title("Change in Average Temperature Each Year")
+        plot_area.set_title(f"Average Temperatures from the %s Dataset" %(regionNames))
+
         #Plotting the error margin. 
         yerr=[]
         #I am adding the annual average uncertainty entries to a list. 
         for entry in  all_centuries_df["uncertainty"]:
             yerr.append(entry)
-        fig = plt.figure()
+        
         #In the expressions x.tolist() and y.tolist(), x and y are are created above when plotting a scatterplot. X is the years columnn of the all_centuries_df dataframe and y is the temperatures.
-        x_axis=x.tolist() 
-        y_axis=y.tolist()
-        axs[1].errorbar(x=x, y=y, yerr=yerr, ls='none') #Plotting the errorbar onto the second subplot at the second row.
-        axs[1].set_title("Average Uncertainty Each Year")
-        axs[1].set_xlabel("Year")
-        axs[1].set_ylabel("Average Uncertainty")
-        plt.show()
-        print("The coefficient of determination is", r_sq)
-        print ("The data above is from the", type_data,"data set.")
+        uncertainAx[row][col].errorbar(x=x, y=y, yerr=yerr, ls='none') #Plotting the errorbar onto the second subplot at the second row.
+        uncertainAx[row][col].set_title(f"Average Uncertainty from the %s Dataset" %(regionNames))
+        uncertainAx[row][col].set_xlabel("Year")
+        uncertainAx[row][col].set_ylabel("Average Uncertainty")
+
         #This works properly because yerr, x, and y all come from the same DataFrame. Consequently, they will be sorted in the same way and thus line up with each other.
 
 
