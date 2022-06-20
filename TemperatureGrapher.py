9f36eb57bf7565e5eed853094e3ab60e4578033e
Michael Rehani <90718054+mrrehani@users.noreply.github.com>
2022-06-20 10:29:58 -0500

    Add files via upload

--- TemperatureGrapher.py
+++ TemperatureGrapher.py
@@ -93,3 +93,54 @@ class TemperatureGrapher():
         print("The coefficient of determination is", r_sq)
         print ("The data above is from the", type_data,"data set.")
         #This works properly because yerr, x, and y allcome from the same DataFrame. Consequently, they will be sorted in the same way and thus line up with each other.
+
+
+    def group_by_century(self,df, type_place):
+        curr_place=None
+        century_began={1700:[], 1750:[],1800:[], 1850:[],1900:[], 1950:[],2000:[]} #Each place will be placed in a list depending on when it began recording temperatures.
+        index=1
+        avg_temps=df.groupby(type_place).mean()
+        avg_temps['AverageTemperature'] = avg_temps['AverageTemperature'].replace(np.nan, 0)
+        for column in df: #This loop will figure out the indeces of the "dt" column and the place name column. 
+            if column=="dt":
+                dt_index=index
+            elif column==type_place:
+                place_index=index
+            index+=1
+        index=0
+        for row in df.itertuples(): #This will iterate through each entry in the data frame. 
+            dt,place=row[dt_index],row[place_index]
+            if curr_place==place: #If curr_place==place, then we are not at the place's first entry (when they began recording temperatures). We only need the first entry of each place.
+                continue
+            elif curr_place==None or curr_place!=place: 
+                curr_place=place
+                year_began=int(dt[:4])#This will record the year of the first year a place began recording temperatures.
+            #The conditionals below will classify the place based on when it began recording temperatures.
+            if int(str(year_began)[2])<5:#If the third number of the year is less than 5 (e.g. 1846 4<5).
+                century_began[int(str(year_began)[:2]+"00")].append([curr_place,index,avg_temps.loc[curr_place]["AverageTemperature"]])#This takes the first two numbers of the yera (which tells us the century) and adds 00 to it. It needs to temporarily be a string to concatonate the two. E.g. 1846 will become 1800.
+            else:
+                century_began[int(str(year_began)[:2]+"50")].append([curr_place,index,avg_temps.loc[curr_place]["AverageTemperature"]])
+            index+=1
+        return century_began
+
+    def highest_avg(self,df, type_place, row, col, axs):
+        century_began = self.group_by_century(df,type_place)
+        highest_places,highest_temps=[],[]
+        for century in century_began:
+            if len(century_began[century])>0:#If no areas began recording temperatures after a specific time period like 2000, the list would be empty.
+                sorted_list=sorted(century_began[century], key = lambda x: int(x[2]), reverse=True) #Sorts the list by average temperature. It's fine to hardcode the index of average temperature because the code that creates the list ensures that an average temperature is always recorded.
+                #If a place had no recorded temperatures (resulting in the average temperature being NaN), it was replaced with 0.0 in the group_by_century function to act as a placeholder of sorts. 
+                if sorted_list[0][2]>0.0: 
+                    highest_places.append(sorted_list[0][0])
+                    highest_temps.append(sorted_list[0][2])
+        
+        bar_top_temps = axs[row][col].bar(highest_places, highest_temps, color=['b','r','g','orange','purple','pink','orange'])
+        #The code below is from https://stackoverflow.com/questions/57340415/matplotlib-bar-plot-add-legend-from-categories-dataframe-column
+        colors = {'1700':'blue', '1750':'red', '1800':'green','1850':'orange', '1900': 'purple', '1950': 'pink', '2000': "yellow"}         
+        labels = list(colors.keys())
+        handles = [plt.Rectangle((0,0),1,1, color=colors[label]) for label in labels]
+        #The code below is from https://stackoverflow.com/questions/4700614/how-to-put-the-legend-out-of-the-plot
+        plt.legend(handles, labels, loc='center left', bbox_to_anchor=(1.05, 1),prop={'size': 12.5})
+        axs[row][col].set(xlabel=type_place, ylabel='Temperature')
+        axs[row][col].set_xticks(range(len(highest_places))) #This sets the tick locations based on how many entries are in the list.
+        axs[row][col].set_xticklabels(labels=highest_places,rotation=25)
